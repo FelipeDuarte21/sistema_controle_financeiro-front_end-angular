5d5465a02cc894d77f20746b7de3ca267fc4f733
Felipe Duarte <felipe15lfde@gmail.com>
2021-07-05 23:40:09 -0300

    Implementando a classe LancamentoService

--- src/app/servicos/lancamento.service.ts
+++ src/app/servicos/lancamento.service.ts
@@ -1,29 +1,32 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 import { Lancamento } from "../modelos/lancamento.model";
 
 @Injectable()
 export class LancamentoService{
+
+    private baseURL:string = `${environment.apiURL}/lancamento`;
 
     constructor(
         private http: HttpClient
     ){}
 
     public cadastrar(lancamento: Lancamento):Observable<Lancamento>{
-        return null;
+        return this.http.post<Lancamento>(this.baseURL,lancamento);
     }
 
     public alterar(lancamento: Lancamento):Observable<Lancamento>{
-        return null;
+        return this.http.put<Lancamento>(this.baseURL,lancamento);
     }
 
     public excluir(id: number):Observable<any>{
-        return null;
+        return this.http.delete(`${this.baseURL}/${id}`);
     }
 
     public buscarPorId(id:number):Observable<Lancamento>{
-        return null;
+        return this.http.get<Lancamento>(`${this.baseURL}/${id}`);
     }
 
 }
