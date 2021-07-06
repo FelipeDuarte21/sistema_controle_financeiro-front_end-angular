e7924fca033b701087fdcdf309377ccd6d5d7093
Felipe Duarte <felipe15lfde@gmail.com>
2021-07-05 23:42:05 -0300

    Implementando a classe TipoService

--- src/app/servicos/tipo.service.ts
+++ src/app/servicos/tipo.service.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 import { Tipo } from "../modelos/tipo.model";
 
 @Injectable()
 export class TipoService{
+
+    private baseURL:string = `${environment.apiURL}/tipoLancamento`
 
     constructor(
         private http: HttpClient
     ){}
 
     public buscarTodos():Observable<Tipo>{
-        return null;
+        return this.http.get<Tipo>(this.baseURL);
     }
 
 }
