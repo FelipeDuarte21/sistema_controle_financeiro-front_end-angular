6e25310eb6eef7c8dfb7301bcbae1da322dec6e6
Felipe Duarte <felipe15lfde@gmail.com>
2021-07-05 23:36:16 -0300

    Implementando a classe BalancoService

--- src/app/servicos/balanco.service.ts
+++ src/app/servicos/balanco.service.ts
@@ -1,21 +1,24 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 import { Balanco } from "../modelos/balanco.model";
 
 @Injectable()
 export class BalancoService{
 
+    private baseURL:string = `${environment.apiURL}/balanco`;
+
     constructor(
         private http: HttpClient
     ){}
 
-    public buscarAtual():Observable<Balanco>{
-        return null;
+    public buscarAtual(idCategoria:number):Observable<Balanco>{
+        return this.http.get<Balanco>(`${this.baseURL}/atual?categoria=${idCategoria}`);
     }
 
-    public buscarPorData(mes:number,ano:number):Observable<Balanco>{
-        return null;
+    public buscarPorData(idCategoria:number, mes:number,ano:number):Observable<Balanco>{
+        return this.http.get<Balanco>(`${this.baseURL}/data?categoria=${idCategoria}&mes=${mes}&ano=${ano}`);
     }
 
 }
